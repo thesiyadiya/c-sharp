fa6b491a07f5220a16c305290a6650d33ab96d44
thesiyadiya <diyapatel9412@gmail.com>
2025-02-01 15:15:34 +0530

    Add files via upload

---
+++ helloworld.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.Write("Hello World");
+            Console.Read();
+        }
+    }
+}
