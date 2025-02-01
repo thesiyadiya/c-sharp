c57906dab3b80f32c5e452bffea11b95c4185de2
thesiyadiya <diyapatel9412@gmail.com>
2025-02-01 15:32:51 +0530

    Add files via upload

---
+++ marksheet.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int m1, m2, total;
+            double per;
+            string grade, result;
+            Console.WriteLine("Enter marks 1:");
+            m1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter marks 2:");
+            m2=Convert.ToInt32(Console.ReadLine());
+            total = m1 + m2;
+            if(m1>=40 && m2>=40)
+            {
+                result = "PASS";
+                per = Convert.ToDouble(total) / 2;
+                if (per >= 70)
+                {
+                    grade = "A";
+                }
+                else if(per>=60)
+                {
+                    grade = "B";
+                }
+                else if(per>=50)
+                {
+                    grade = "C";
+                }
+                else
+                {
+                    grade="D";
+                }
+            }
+            else
+            {
+                result = "FAIL";
+                per = 0;
+                grade = "F";
+            }
+            Console.WriteLine("Toatal:" + total);
+            Console.WriteLine("Percentage:" + per);
+            Console.WriteLine("Result:"+result);
+            Console.WriteLine("Grade:"+grade);
+            Console.Read();
+        }
+    }
+}
