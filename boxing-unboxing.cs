ee4543a0fe7c6547f4707a42159db734e2ebbe60
thesiyadiya <diyapatel9412@gmail.com>
2025-02-07 16:46:09 +0530

    Add files via upload

---
+++ boxing-unboxing.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            object obj = i;
+            Console.Write(obj);
+            int j = (int)obj;
+            Console.Write(j);
+            Console.Read();
+        }
+    }
+}
