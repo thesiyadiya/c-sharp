9f727e26259332c11cf442c0d4a9369059746dcd
thesiyadiya <diyapatel9412@gmail.com>
2025-02-07 16:57:20 +0530

    Add files via upload

---
+++ 1Darray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            for (int a = 0; a < arr.Length; a++)
+            {
+                Console.Write("Enter value:");
+                arr[a] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach(int b in arr)
+            {
+                Console.WriteLine(b);
+            }
+            Console.Read();
+        }
+    }
+}
