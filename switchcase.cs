e030fcf2984e6578f313b3234935d924880570fd
thesiyadiya <diyapatel9412@gmail.com>
2025-03-22 10:32:00 +0530

    Add files via upload

---
+++ switchcase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace switchcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 11;
+            switch (i)
+            {
+                case 1:
+                    Console.WriteLine("Pass");
+                    break;
+                case 10:
+                    Console.WriteLine("Fail");
+                    break;
+                default:
+                    Console.WriteLine("Invalid");
+                    break;
+            }
+            Console.Read();
+        }
+    }
+}
