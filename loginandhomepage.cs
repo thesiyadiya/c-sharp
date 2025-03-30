6f1521587f390a2b53040fbb7e14bbc1beec1bf1
thesiyadiya <diyapatel9412@gmail.com>
2025-03-30 10:59:43 +0530

    Add files via upload

---
+++ loginandhomepage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace loginandhomepage
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection cn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\lgn.mdf;Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String Sql = "select * from login where username='" + unm.Text + "'and password='" + pwd.Text + "'";
+            SqlDataAdapter da = new SqlDataAdapter(Sql, cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count == 1)
+            {
+                Home F1 = new Home();
+                F1.Show();
+                this.Hide();
+            }
+        }
+    }
+
+    
+    }
+
