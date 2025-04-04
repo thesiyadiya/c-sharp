f055e23698248225efc08d53468b9ee60bf0a353
thesiyadiya <diyapatel9412@gmail.com>
2025-04-04 16:54:31 +0530

    Add files via upload

---
+++ constructeroverloading.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace constructeroverloading
+{
+    class constructer
+    {
+        public constructer(int i, int j)
+        {
+            Console.WriteLine(i + j);
+        }
+        public constructer(string result, string grade)
+        {
+            Console.WriteLine(result + "," + grade);
+        }
+        internal class program
+        {
+            static void Main(string[] args)
+            {
+                constructer a = new constructer(10, 20);
+                constructer a1 = new constructer("FAIL", "F");
+                Console.Read();
+            }
+        }
+    }
+}
---
+++ refandout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refout
+{
+    class refout
+    {
+        public int outparameter(out int a, out int b)
+        {
+            a = 20;
+            b = 20;
+            return a + b;
+        }
+        public int refparameter(ref int a, ref int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            int i = 10;
+            int j = 20;
+            refout ro = new refout();
+            Console.Write(ro.outparameter(out i, out j));
+            Console.Write(ro.refparameter(ref i, ref j));
+            Console.Read();
+        }
+    }
+}
