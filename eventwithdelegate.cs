25f71d340d33c8ed1111f09e3478c207f28d2dd8
thesiyadiya <diyapatel9412@gmail.com>
2025-04-04 17:10:46 +0530

    Add files via upload

---
+++ eventwithdelegate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace @event
+{
+    class Program
+    {
+        public delegate void EventHandler(string a);
+
+        public class Operation
+        {
+            public event EventHandler xyz;
+            public void Action(string a)
+            {
+                if (xyz != null)
+                {
+                    xyz(a);
+                    Console.WriteLine(a);
+                }
+                else
+                {
+                    Console.WriteLine("Not Registered");
+                }
+            }
+        }
+        public static void CatchEvent(string s)
+        {
+            Console.WriteLine("Method Calling");
+        }
+
+        static void Main(string[] args)
+        {
+            Operation o = new Operation();
+            o.xyz += new EventHandler(CatchEvent);
+            o.Action("Event Calling");
+            Console.ReadLine();
+        }
+    }
+}
