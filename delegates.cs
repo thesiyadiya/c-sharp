e83ab1fd9e6f8755f91b6cc810fe53d54a67b606
thesiyadiya <diyapatel9412@gmail.com>
2025-04-05 10:20:10 +0530

    Add files via upload

---
+++ delegates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    internal class Program
+    {
+        public delegate void addnum(int a, int b);
+        public delegate void subnum(int a, int b);
+
+        public void sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public void sub(int a,int b)
+        {
+            Console.WriteLine(a - b);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            addnum del1 = new addnum(obj.sum);
+            subnum del2 = new subnum(obj.sub);
+            del1(10, 5);
+            del2(10, 5);
+            Console.Read();
+        }
+    }
+}
+/*
+15
+5
+*/
