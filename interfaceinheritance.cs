f762ca6c6d1ac022b73a0908d31029ff452f7273
thesiyadiya <diyapatel9412@gmail.com>
2025-04-05 09:39:31 +0530

    Add files via upload

---
+++ interfaceinheritance.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfaceinheritance
+{
+    interface result
+    {
+        void grade();
+    }
+    class sem4:result
+    {
+        public void grade()
+        {
+            Console.WriteLine("P");
+        }
+    }
+     internal class program
+    {
+        static  void Main(string[] args)
+        {
+            sem4 s = new sem4();
+            s.grade();
+            Console.Read();
+        }
+    }
+}
+/*
+P
+*/
+
