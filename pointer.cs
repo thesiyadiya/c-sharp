d59d9fa842d56cf4b80a2e85509bc65b4076be4c
thesiyadiya <diyapatel9412@gmail.com>
2025-04-05 09:46:00 +0530

    Add files via upload

---
+++ pointer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pointer
+{
+    internal class Program
+    {
+        static unsafe void Main(string[] args)
+        {
+            int i = 10;
+            int* x = &i;
+            Console.WriteLine(i);
+            Console.WriteLine((int)x);
+            Console.Read();
+        }
+    }
+}
+/*
+10
+123531080
+*/
