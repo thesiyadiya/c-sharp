09c7600de2f14bcc50f3651b71a8b8bade53ce84
thesiyadiya <diyapatel9412@gmail.com>
2025-04-05 10:05:04 +0530

    Add files via upload

---
+++ properties.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace properties
+{
+    class result
+    {
+        public string grade;
+        public string Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+    }
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            result res1 = new result();
+            res1.Grade = "P";
+            Console.WriteLine(res1.Grade);
+            Console.Read();
+        }
+    }
+}
+/*
+P
+*/
